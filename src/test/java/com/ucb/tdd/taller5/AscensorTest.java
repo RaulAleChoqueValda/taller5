1601797a1de9d5ef4ee34e22cdb913dabf5b26b5
Raul Alejandro Choque Valda <raul.choque@ucb.edu.bo>
2022-10-20 03:02:40 -0400

    Primer Commit

---
+++ src/test/java/com/ucb/tdd/taller5/AscensorTest.java
@@ -0,0 +1,21 @@
+package com.ucb.tdd.taller5;
+
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+
+import org.junit.rules.ExpectedException;
+
+import org.junit.Test;
+
+import org.junit.Before;
+
+public class AscensorTest {
+
+    @Test
+    void test() {
+
+    }
+}
---
+++ src/test/java/com/ucb/tdd/taller5/Taller5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucb.tdd.taller5;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class Taller5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
